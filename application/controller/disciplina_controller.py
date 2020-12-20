f66ae33e2840aead2f799705975a2055296e2371
kaslufl <leallucasfonseca@gmail.com>
2020-12-20 12:28:20 -0300

    fix: Corrige controladores disciplina e home

--- application/controller/disciplina_controller.py
+++ application/controller/disciplina_controller.py
@@ -1,8 +1,10 @@
 from application import app
 from flask import render_template, request
+from application import avaDAO
 # importar as classes que são usadas na página
 
 @app.route("/disciplina/<codigo_disciplina>")
 def disciplina(codigo_disciplina):
-
-    return render_template("disciplina.html")
+    disciplina = avaDAO.get_disciplina_by_id(int(codigo_disciplina))
+    aulas = disciplina.get_aulas()
+    return render_template("disciplina.html", disciplina = disciplina, aulas = aulas)
--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,8 +1,12 @@
 from application import app
 from flask import render_template, request
+from application.model.entity.disciplina import Disciplina
+from application.model.entity.aula import Aula
+from application import avaDAO
 # importar as classes que são usadas na página
 
 @app.route("/")
 @app.route("/home")
-def home(): 
-    return render_template("home.html")
+def home():
+    lista_disciplinas = avaDAO.listar_todas_disciplinas()
+    return render_template("home.html", lista_disciplinas = lista_disciplinas)
